d81158cb990e50f641dedb1aceede1c9573d3b3f
Xeronade17 <xero11712@gmail.com>
2026-07-23 22:56:12 +0800

    Implement jump and movement functions for dino

--- script.js
+++ script.js
@@ -1,26 +1,93 @@
 let dino = document.getElementById("dino");
 
-
-document.addEventListener("keydown", function(event){
-
-if(event.code === "Space"){
-
-jump();
-
-}
-
-});
+let position = 80;
+let jumping = false;
+let score = 0;
 
 
 function jump(){
 
-dino.style.bottom = "80px";
+    if(jumping) return;
+
+    jumping = true;
+
+    let height = 0;
+
+    let up = setInterval(()=>{
+
+        height += 5;
+        dino.style.bottom = (50 + height) + "px";
 
 
-setTimeout(function(){
+        if(height >= 100){
 
-dino.style.bottom = "0px";
+            clearInterval(up);
+
+
+            let down = setInterval(()=>{
+
+                height -= 5;
+                dino.style.bottom = (50 + height) + "px";
+
+
+                if(height <= 0){
+
+                    clearInterval(down);
+                    jumping=false;
+
+                }
+
+            },20);
+
+        }
+
+    },20);
+
+}
+
+
+
+function moveLeft(){
+
+    position -= 20;
+    dino.style.left = position+"px";
+
+}
+
+
+function moveRight(){
+
+    position += 20;
+    dino.style.left = position+"px";
+
+}
+
+
+
+setInterval(()=>{
+
+    score++;
+    document.getElementById("score").innerHTML =
+    "Score: " + score;
 
 },500);
 
-}
+
+
+document.addEventListener("keydown",(e)=>{
+
+    if(e.code==="Space"){
+        jump();
+    }
+
+
+    if(e.code==="ArrowLeft"){
+        moveLeft();
+    }
+
+
+    if(e.code==="ArrowRight"){
+        moveRight();
+    }
+
+});
