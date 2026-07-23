d811c8882a8af8fd67ac37e13692decd90841a41
Xeronade17 <xero11712@gmail.com>
2026-07-23 23:18:50 +0800

    Refactor CSS styles for consistency and readability

--- style.cs
+++ style.cs
@@ -1,91 +1,90 @@
-body {
-    margin: 0;
-    background: black;
-    font-family: Arial;
-    overflow: hidden;
+body{
+margin:0;
+background:black;
+font-family:Arial;
 }
 
 
-.game {
-    width: 100%;
-    height: 350px;
-    background: linear-gradient(#050816, #20284f);
-    position: relative;
-    overflow: hidden;
+.game{
+height:350px;
+width:100%;
+background:linear-gradient(#05051a,#202040);
+position:relative;
+overflow:hidden;
 }
 
 
-.moon {
-    width: 70px;
-    height: 70px;
-    background: #fff2b2;
-    border-radius: 50%;
-    position: absolute;
-    top: 30px;
-    right: 50px;
+.moon{
+width:70px;
+height:70px;
+background:#fff3b0;
+border-radius:50%;
+position:absolute;
+right:50px;
+top:30px;
 }
 
 
-.star {
-    width: 5px;
-    height: 5px;
-    background: white;
-    border-radius: 50%;
-    position: absolute;
+.stars{
+position:absolute;
+width:100%;
+height:100%;
+background-image:
+radial-gradient(white 2px,transparent 2px);
+background-size:80px 80px;
 }
 
 
-.star1 {
-    top: 80px;
-    left: 100px;
-}
-
-.star2 {
-    top: 120px;
-    left: 250px;
+#score{
+color:white;
+padding:15px;
+font-size:20px;
 }
 
 
-#score {
-    color: white;
-    padding: 20px;
-    font-size: 20px;
+#dino{
+
+position:absolute;
+bottom:50px;
+left:80px;
+font-size:50px;
+
 }
 
 
-#dino {
-    position: absolute;
-    bottom: 50px;
-    left: 80px;
-    font-size: 50px;
+#obstacle{
+
+position:absolute;
+bottom:50px;
+right:-50px;
+font-size:45px;
+
 }
 
 
-#ground {
-    position:absolute;
-    bottom:0;
-    width:100%;
-    height:50px;
-    background:#111;
+#ground{
+
+position:absolute;
+bottom:0;
+height:50px;
+width:100%;
+background:#111;
+
 }
 
 
-#cactus {
-    position:absolute;
-    bottom:50px;
-    right:0;
-    font-size:45px;
+.buttons{
+
+text-align:center;
+margin-top:20px;
+
 }
 
 
-.controls {
-    text-align:center;
-    margin-top:20px;
-}
+button{
 
+font-size:25px;
+padding:10px 20px;
+margin:5px;
 
-button {
-    font-size:20px;
-    padding:10px 20px;
-    margin:5px;
 }
